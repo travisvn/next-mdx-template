95058208811f998416daff36f750bf46f50588ad
Travis V <github@travis.engineer>
2024-05-04 17:55:20 -0400

    Updated site.ts config with sample deployment

--- config/site.ts
+++ config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   title: 'My MDX Blog',
   description: 'Next.js and MDX content',
-  baseUrl: (process.env.BASE_URL != null && process.env.BASE_URL != '') ? process.env.BASE_URL : `http://localhost:3000`,
+  baseUrl: (process.env.BASE_URL != null && process.env.BASE_URL != '') ? process.env.BASE_URL : `https://next-mdx-template.vercel.app`,
 }
 
 export const markdownDirectory = 'content'
